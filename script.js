f413efb10e789b1ad8e26c4fc93284b2147e7ae3
Emmanuel Koech <koechemmanuel2002@gmail.com>
2022-11-12 14:43:21 +0300

    Concat method in Arrays

--- script.js
+++ script.js
@@ -81,5 +81,11 @@ letters = [`a`,`b`,`c`,`d`,`e`];
 console.log(letters.reverse())
 console.log(letters)
 
+let  num1 = [`1`,`2`,`3`,`4`,`5`];
+let  num2 = [`6`,`7`,`8`,`9`,`10`];
+let  num3 = [`11`,`12`,`13`,`14`,`15`]
+let allNum =num1.concat(num1,num2,num3);
 
+
+console.log(allNum)
 /////////////////////////////////////////////////
