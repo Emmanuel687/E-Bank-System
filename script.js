ba9ef9c3cf02e83b0fe80b3552157d285272034d
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2022-11-14 17:02:01 +0300

    Account 1 Movement loop

--- script.js
+++ script.js
@@ -61,6 +61,14 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = (movements)=>{
+  movements.forEach((mov,i)=>{
+
+  })
+
+
+}
+displayMovements(account1.movements)
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
