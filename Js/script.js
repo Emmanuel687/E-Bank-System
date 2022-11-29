755db6e974ad96681373fcfbc460cdfad5c17304
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2022-11-29 15:37:32 +0300

    Deleting Current Account

--- Js/script.js
+++ Js/script.js
@@ -191,8 +191,9 @@ btnTransfer.addEventListener('click', function (e) {
 btnClose.addEventListener(`click`, function(e){
   e.preventDefault()
   if(inputCloseUsername.value===currentAccount.username && Number(inputClosePin.value)===currentAccount.pin){
-    const index = accounts.findIndex(acc=>acc.username===currentAccount.username)
-    console.log(index)
+    const index = accounts.findIndex(
+      acc=>acc.username===currentAccount.username)
+    accounts.splice(index,1)
   }
 })  
 
