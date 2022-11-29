0593dc0b3b3fac00d79e016be741633669f0ee88
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2022-11-29 14:19:24 +0300

    Added eventlistener to button close

--- Js/script.js
+++ Js/script.js
@@ -188,6 +188,11 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnClose.addEventListener(`click`, function(e){
+  e.preventDefault()
+  console.log(`Hello World`)
+})
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
