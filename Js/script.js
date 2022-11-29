7ae25e95bbc1009840f12cb842cd87bb653d48ce
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2022-11-29 14:32:46 +0300

    Created a call back function to remove account

--- Js/script.js
+++ Js/script.js
@@ -190,8 +190,11 @@ btnTransfer.addEventListener('click', function (e) {
 
 btnClose.addEventListener(`click`, function(e){
   e.preventDefault()
-  console.log(`Hello World`)
-})
+  if(inputCloseUsername.value===currentAccount.username && Number(inputClosePin.value)===currentAccount.pin){
+    const index = accounts.findIndex(acc=>acc.username===currentAccount.username)
+    console.log(index)
+  }
+})  
 
 
 /////////////////////////////////////////////////
