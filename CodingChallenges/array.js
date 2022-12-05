83f21e42d8ee5b017187a9eaa59ec69b24cced7e
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2022-12-05 20:16:04 +0300

    Removed Arrays

--- CodingChallenges/array.js
+++
@@ -1,8 +0,0 @@
-function position(letter){
-    //Write your own Code!
-      let lettersArr = letter.split(" ");
-      let indexs = lettersArr.forEach((index => `Position of alphabet: ${index+1}`));
-      console.log(indexs)
-     }
-
-     position(`HelloWorld`)
